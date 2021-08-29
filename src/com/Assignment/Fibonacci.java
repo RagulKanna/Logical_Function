a9fe39ca625c2b2c0f3032f01ecc990dda5303ba
RagulKanna <ragulragi619@gmail.com>
2021-08-29 23:24:07 +0530

    Logical Programs

---
+++ src/com/Assignment/Fibonacci.java
@@ -0,0 +1,23 @@
+package com.Assignment;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner fi = new Scanner(System.in);
+        System.out.println("Enter the no of terms to find fibonacci series:");
+        int n = fi.nextInt();
+        int n1 = 0;
+        int n2 = 1;
+        System.out.println("The Fibonacci Series for "+n+" terms are");
+        System.out.println(n1);
+        System.out.println(n2);
+        for (int i = 2; i <n;++i) {
+            int n3=n1+n2;
+            n1=n2;
+            n2=n3;
+            System.out.println(n3);
+        }
+
+    }
+}
