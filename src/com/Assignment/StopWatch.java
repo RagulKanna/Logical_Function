ff51746b7aae77e0894c07396bfe94a531ddcdc6
RagulKanna <ragulragi619@gmail.com>
2021-08-29 23:40:07 +0530

    To calculate elapsedtime

---
+++ src/com/Assignment/StopWatch.java
@@ -0,0 +1,18 @@
+package com.Assignment;
+
+import java.util.Scanner;
+
+public class StopWatch {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Type ' r ' to Start :");
+        long start = System.currentTimeMillis();
+        sc.nextLine();
+        System.out.print("Type ' s ' to Stop :");
+        sc.nextLine();
+        long stop = System.currentTimeMillis();
+        long Elapsedtime = (stop - start) / 1000;
+        System.out.print("Elapsed Time was: "+Elapsedtime+" Seconds");
+    }
+}
